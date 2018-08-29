488c5380436cd82ffa4213c0025429d4891c5f52
Aline Sotte <alinedpaso@gmailcom>
2018-08-29 20:35:36 -0300

    add notificaçoes para o estado da ProessorAct

--- app/src/main/java/br/ufjf/dcc196/exemplo2/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo2/MainActivity.java
@@ -5,23 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnProfessor;
-
+    public static final String Pessoa_Nome="nome";
+    private Button btnProfessor;
+    private EditText edNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        edNome=findViewById(R.id.edt_nome);
 
         btnProfessor=findViewById(R.id.btn_professor);
         btnProfessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ProfessorActivity.class);
+                intent.putExtra(Pessoa_Nome,edNome.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/br/ufjf/dcc196/exemplo2/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo2/ProfessorActivity.java
@@ -2,12 +2,46 @@ package br.ufjf.dcc196.exemplo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfessorActivity extends AppCompatActivity {
+
+        private TextView txtProfMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+        txtProfMensagem=findViewById(R.id.txt_prof_mensagem);
+        Bundle extras=getIntent().getExtras();
+
+        if(extras!=null) {
+            String nome = extras.getString(MainActivity.Pessoa_Nome);
+            txtProfMensagem.setText("Olá Prof." + nome + "!");
+        }
+
+        Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_SHORT).show();
+
+        @Override
+        protected void onStart(){
+            super.onStart();
+            Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected void onResume(){
+
+            super.onResume();
+            Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onStop(){
+            super.onStop();
+            Toast.makeText(getApplicationContext(),"prof onCreate()", Toast.LENGTH_SHORT).show(); }
+        }
+
     }
 }
