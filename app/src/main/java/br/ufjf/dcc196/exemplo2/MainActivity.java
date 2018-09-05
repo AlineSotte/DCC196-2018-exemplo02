8e120ddbce3ef03a3714e61ea50cfe5792bdf720
Aline Sotte <alinedpaso@gmail.com>
2018-09-05 19:55:16 -0300

    Tela Professor comunicando Main

--- app/src/main/java/br/ufjf/dcc196/exemplo2/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo2/MainActivity.java
@@ -1,5 +1,6 @@
 package br.ufjf.dcc196.exemplo2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,23 +12,40 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String Pessoa_Nome="nome";
+    private static int Request_SIAP=1;
+    public static String PROF_SIAPE="siape";
     private Button btnProfessor;
     private EditText edNome;
+    private TextView txtMensagem;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edNome=findViewById(R.id.edt_nome);
+        txtMensagem=(TextView) findViewById(R.id.txt_prof_mensagem);
+        edNome=(EditText) findViewById(R.id.edt_nome);
 
         btnProfessor=findViewById(R.id.btn_professor);
-        btnProfessor.setOnClickListener(new View.OnClickListener() {
+        btnProfessor.setOnClickListener(
+                new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ProfessorActivity.class);
                 intent.putExtra(Pessoa_Nome,edNome.getText().toString());
-                startActivity(intent);
+                startActivityForResult(intent, MainActivity.Request_SIAP);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==MainActivity.Request_SIAP && resultCode== Activity.RESULT_OK && data!= null){
+
+        Bundle bundleResultado=data.getExtras();
+        String siape=bundleResultado.getString(MainActivity.PROF_SIAPE);
+        txtMensagem.setText("Olá Professor de Siape"+ siape);
+    }}
 }
--- app/src/main/java/br/ufjf/dcc196/exemplo2/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo2/ProfessorActivity.java
@@ -1,13 +1,20 @@
 package br.ufjf.dcc196.exemplo2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfessorActivity extends AppCompatActivity {
 
         private TextView txtProfMensagem;
+        private Button  btnProfConfirma;
+        private EditText edtSiape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +23,23 @@ public class ProfessorActivity extends AppCompatActivity {
         txtProfMensagem = findViewById(R.id.txt_prof_mensagem);
         Bundle extras = getIntent().getExtras();
 
+
+
         if (extras != null) {
             String nome = extras.getString(MainActivity.Pessoa_Nome);
             txtProfMensagem.setText("Olá Prof." + nome + "!");
-        }
+            }
+
+            btnProfConfirma.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent resutado=new Intent();
+                    resutado.putExtra(MainActivity.PROF_SIAPE, edtSiape.getText().toString());
+                    setResult(Activity.RESULT_OK,resutado);
+                    finish();
+                }
+            });
 
         Toast.makeText(getApplicationContext(), "prof onCreate()", Toast.LENGTH_SHORT).show();
 
@@ -43,7 +63,9 @@ public class ProfessorActivity extends AppCompatActivity {
         protected void onStop(){
             super.onStop();
             Toast.makeText(getApplicationContext(),"prof onStop()", Toast.LENGTH_SHORT).show(); }
-        }
+
+
+
 
 }
 
